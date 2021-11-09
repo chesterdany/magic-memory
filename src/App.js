1ba1175c827fb0855c19c33ce2dcc6e5631e1875
Brendea Daniel <chesterdany22@yahoo.com>
2021-11-10 01:42:42 +0200

    use effect with compare function added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Card from './Components/Card';
 import './App.css';
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ];
 
 function App() {
@@ -35,8 +35,13 @@ function App() {
   // compare the choices
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-      choiceOne.src === choiceTwo.src ? console.log('Match!') : console.log('No match!');
-      resetTurn();
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('Match!');
+        resetTurn();
+      } else {
+        console.log('No Match!');
+        resetTurn();
+      }
     }
   }, [choiceOne, choiceTwo]);
 
